1431f603d78d7a55a1d5201a68b3d679d9a868b5
Lluvia Frias <lluvi.frias@gmail.com>
2018-05-09 05:19:29 +0000

    automatically backed up by learn

--- parrot.rb
+++ parrot.rb
@@ -1,8 +1,6 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
-def parrot("Pretty bird!", phrase= "Squawk!")
+def parrot(phrase= "Pretty bird!", "Squawk!")
   puts "Squawk!"
 
 end
-
-parrot
